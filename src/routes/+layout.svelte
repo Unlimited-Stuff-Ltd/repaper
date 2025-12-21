7f9074b3284d7ff49eb9bb2caebb580234e4d134
Theo Kapi <theo_kapi@outlook.com>
2025-12-21 10:43:28 -0800

    No Dark Mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,8 +4,6 @@
 	import { resolve } from '$app/paths';
 
 	let { children } = $props();
-
-	let mode = $state('');
 </script>
 
 <svelte:head>
@@ -13,19 +11,17 @@
 	<title>Repaper</title>
 </svelte:head>
 
-<main class="{mode} h-screen w-screen">
+<main class="h-screen w-screen">
 	<nav class="fixed h-screen w-70 border-r pt-9">
 		<a href={resolve('/')}><h2 class="text-center text-3xl font-black">Repaper</h2></a>
 		<div class="pt-4 pl-7">
-			{#if mode === ''}
-				<button class="a" onclick={() => (mode = 'dark')}>Dark Mode</button>
-			{:else}
-				<button class="a" onclick={() => (mode = '')}>Light Mode</button>
-			{/if}
+			<button class="a">Create a Document</button><br />
 		</div>
 	</nav>
 	<div class="flex">
 		<div class="w-70"></div>
-		<div>{@render children()}</div>
+		<div>
+			{@render children()}
+		</div>
 	</div>
 </main>
